feb5933f15e3b92fbd9eec9fceace1f85c915524
Zinddine <ZinddineGarboudje>
2026-04-21 21:50:30 +0100

    l did : add the functions , so we can use +,- and res

--- SCRs/js1.js
+++ SCRs/js1.js
@@ -1,1 +1,21 @@
+const dec = document.getElementById("dec");
+const res = document.getElementById("res");
+const inc = document.getElementById("inc");
 
+const counter = document.getElementById("counter");
+let count = 0;
+
+dec.onclick = function () {
+  count--;
+  counter.textContent = count;
+};
+
+res.onclick = function () {
+  count = 0;
+  counter.textContent = count;
+};
+
+inc.onclick = function () {
+  count++;
+  counter.textContent = count;
+};
